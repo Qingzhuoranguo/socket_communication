65e53c91ad4d7eb494df9fd7f7f4753e4659f1c4
Aaron <qga6@sfu.ca>
2020-05-26 05:17:18 -0700

    added threading to handle send and recv however not very useful because still only one thread can operate, only useful part is it can rev while typing (but not able to display)

--- client.cpp
+++ client.cpp
@@ -1,21 +1,60 @@
 #include <iostream>
 #include <string.h>
-
+#include <stdio.h>
 #include "AAPS.hpp"
 
+#include <thread>         // std::thread
+#include <mutex>          // std::mutex
+
+
+std::mutex display_lock;
+
+void type (AAPS_COM *c){
+	while(true){
+		AAPS_COM Connection = *c;
+		std::string input;
+
+		display_lock.lock();
+		std::cout << "You>\t";
+		getline (std::cin, input);
+		display_lock.unlock();
+
+		char *msg = (char *)malloc (input.size()+1);
+		memcpy (msg, input.c_str(), input.size()+1);
+		Connection.Send(msg);
+		free(msg);
+	}
+}
+
+void display (AAPS_COM *c){
+	while (true){
+		AAPS_COM Connection = *c;
+		char *rmsg = Connection.Recv();
+
+		display_lock.lock();
+		std::cout <<"Server>\t"<<rmsg << std::endl;
+		display_lock.unlock();
+	}
+}
 
 int main (int argc, char *argv[] ){
+
 	char ip[16];
-	std::cin >> ip;
+	memcpy (ip, argv[1],16);
 	int port;
-	char p[15];
-	std::cin >> p;
-	port = std::stoi(p);
+	port = std::stoi(argv[2]);
 
 
 	AAPS_Socket Server (ip, port);
 	AAPS_COM Connection (&Server);
 	Connection.HandShake();
+
+	std::thread a = std::thread(type,&Connection);
+	std::thread b = std::thread(display, &Connection);
+
+	a.join();
+	b.join();
+
 
 	return 0;
 }
